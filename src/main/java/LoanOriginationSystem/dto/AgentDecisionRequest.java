46fbd084cd200c14050f7f134dfb96f4a9be5726
akshaycharjan <akshaycharjan007@gmail.com>
2026-03-12 14:47:50 +0530

    Add AgentDecisionRequest and added decision enums to ApplicationStatus

---
+++ src/main/java/LoanOriginationSystem/dto/AgentDecisionRequest.java
@@ -0,0 +1,10 @@
+package LoanOriginationSystem.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AgentDecisionRequest {
+    private String decision;
+}
--- src/main/java/LoanOriginationSystem/enums/ApplicationStatus.java
+++ src/main/java/LoanOriginationSystem/enums/ApplicationStatus.java
@@ -4,5 +4,7 @@ public enum ApplicationStatus {
     APPLIED,
     APPROVED_BY_SYSTEM,
     REJECTED_BY_SYSTEM,
-    UNDER_REVIEW
+    UNDER_REVIEW,
+    APPROVED_BY_AGENT,
+    REJECTED_BY_AGENT
 }
